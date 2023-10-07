fb4b57a67915ee290f056aa0937114a7d834abb6
mushroom0322 <144417500+mushroom0322@users.noreply.github.com>
2023-10-07 15:01:24 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h> //需要包含这个头文件
+#include <time.h> // 包含time头文件
 
 void drawMushroom() {
-    printf("     ______     \n");
-    printf("    /       \\   \n");
-    printf("   |         |  \n");
-    printf("   ___________  \n");
-    printf("     | OvO |     \n");
+	printf("   ______    \n");
+	printf("  /       \\ \n");
+	printf(" |         | \n");
+	printf(" ___________ \n");
+	printf("   | OvO  |  \n");
 
 }
 
-int main()
-{
-	printf("Hello world ,I am mushroom!"(>ω<)");
-        drawMushroom();
+int main() {
+ // 使用当前时间作为随机数生成器的种子
+    srand(time(NULL));
+
+	printf("Hello world ,I am mushroom! This is a lusky number:%d\n", rand() % 10); //输出随机个位数
+
+	drawMushroom();
+
 	return 0;
+
 }
